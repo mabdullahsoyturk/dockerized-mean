180e6a6da4ff880dfc5c393ec347521b3946bb80
mabdullahsoyturk <muhammetabdullah.soyturk@agu.edu.tr>
2020-05-10 15:11:26 +0300

    add jwt token

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -27,6 +28,36 @@ router.post("/signup", (req,res,next) => {
             })
           });
         });
+});
+
+router.post("/login", (req,res,next) => {
+  let fetchedUser;
+  User.findOne({email: req.body.email})
+      .then(user => {
+          if(!user) {
+            return res.status(401).json({
+              message: "Auth failed"
+            })
+          }
+          fetchedUser = user;
+          return bcrypt.compare(req.body.password, user.password);
+      })
+      .then(result => {
+        if(!result) {
+          return res.status(401).json({
+            message: "Auth failed"
+          })
+        }
+        const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id}, "secret_string_to_create_token", {expiresIn: "1h"});
+        res.status(200).json({
+          token: token
+        });
+      })
+      .catch(err => {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      });
 });
 
 router.get("", (req, res, next) => {
--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -14,4 +14,12 @@ export class AuthService {
             console.log(response);
           });
   }
+
+  loginUser(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
+    this.httpClient.post("http://localhost:3000/api/users/login", authData)
+        .subscribe(response => {
+          console.log(response);
+        });
+  }
 }
